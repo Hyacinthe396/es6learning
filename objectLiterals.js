d827b6dca938809682d4f4f4d8bf3462a0c5d018
Hyacinthe396 <hmutoni520@daviscollege.com>
2020-12-28 10:51:37 +0200

    object literals

---
+++ objectLiterals.js
@@ -0,0 +1,7 @@
+const technology = {
+    programming: {languages: ["JavaScript", "php", "html", "C"]},
+    softwares: {tools: ["vscode", "notepad"]}
+}
+console.log(technology.programming);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#Object_literals
