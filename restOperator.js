2d914d7bbfcce53beed05ffa52fae85e2889d50a
Hyacinthe396 <hmutoni520@daviscollege.com>
2020-12-28 23:54:59 +0200

    rest operators

---
+++ restOperator.js
@@ -0,0 +1,11 @@
+function sum(...numbers) {
+    // return numbers.reduce((previous, current) => {
+    // //   return previous + current;
+    // return numbers;
+    // });
+    return numbers;
+  }
+  
+  console.log(sum(1, 2, 3));
+  console.log(sum(1, 2, 3, 4));
+  
