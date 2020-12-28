ac436759397a8641b68a9aafe6467d68c763aa9f
Hyacinthe396 <hmutoni520@daviscollege.com>
2020-12-28 10:01:07 +0200

    object destructuring

---
+++ objectDestructuring.js
@@ -0,0 +1,14 @@
+const User = {
+    fname: "Hyacinthe",
+    lname: "Mutoni",
+    location: "Karama",
+    fNumb: "0764834998",
+    isVerified: true,
+    gender: "female"
+}
+
+const { lname,location} = User;
+console.log(lname,location);
+console.log(User);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
