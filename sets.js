a4bea67a3a64d832bea47e9b173a82762e341195
Hyacinthe396 <hmutoni520@daviscollege.com>
2020-12-29 22:32:19 +0200

    addition

--- sets.js
+++ sets.js
@@ -8,12 +8,31 @@
 
  //example 2
 
-var set1 = new Set(); 
-set1.add(10); 
-set1.add(20); 
-set1.add(30).add(40).add(50); 
-console.log(set1); 
+// var set1 = new Set(); 
+// set1.add(10); 
+// set1.add(20); 
+// set1.add(30).add(40).add(50); 
+// console.log(set1); 
 
+
+//third example
+
+// using Set.protoype.delete(value) 
+// creating set it contains 
+// f, o , d, i, e 
+const set1 = new Set("foooodiiiieee"); 
+  
+// deleting e from the set 
+// it prints true 
+console.log(set1.delete('e')); 
+  
+// set contains f, o, d, i 
+console.log(set1); 
+  
+// deleting an element which is  
+// not in the set 
+// prints false 
+console.log(set1.delete('g')); 
 
 
 
